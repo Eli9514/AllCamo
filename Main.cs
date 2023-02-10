4f623ffe23ee8c964a26c6a8488e0696879e5a95
Eli9514 <111830502+Eli9514@users.noreply.github.com>
2023-02-10 17:05:20 -0500

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,24 @@
+﻿using MelonLoader;
+using AllCamo;
+using BTD_Mod_Helper;
+using HarmonyLib;
+using Il2CppAssets.Scripts.Simulation.Bloons;
+using Il2CppAssets.Scripts.Unity.Display;
+using Il2CppAssets.Scripts.Unity.UI_New.InGame;
+
+
+[assembly: MelonInfo(typeof(AllCamo.Main), ModHelperData.Name, ModHelperData.Version, ModHelperData.RepoOwner)]
+[assembly: MelonGame("Ninja Kiwi", "BloonsTD6")]
+
+namespace AllCamo;
+
+[HarmonyPatch]
+public class Main : BloonsTD6Mod
+{
+    [HarmonyPatch(typeof(Factory.__c__DisplayClass21_0), nameof(Factory.__c__DisplayClass21_0._CreateAsync_b__0))]
+    [HarmonyPrefix]
+    static bool Factory_CreateAsync()
+    {
+        return false;
+    }
+}
--- ModHelperData.cs
+++ ModHelperData.cs
@@ -1,13 +1,10 @@
-﻿#nullable enable
-namespace AllCamo
+﻿namespace AllCamo;
+
+public static class ModHelperData
 {
-  public static class ModHelperData
-  {
     public const string Version = "2.1.0";
     public const string Name = "All Camo";
     public const string Description = "Makes all towers, projectiles, bloons, and abilities invis!";
     public const string RepoOwner = "Eli9514";
-    public const string RepoName = "AllCamo";
-    public const string Icon = "ShimmerIcon.png";
-  }
+    public const string Icon = "Icon.png";
 }
